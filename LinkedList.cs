5fadcb89362bc245b58546ab62b9e65d8c94f70d
Hendrik-Amamm <midqav-cYdty6-pevhek>
2022-03-15 16:28:31 +0100

    Erstes Einchecken des Moduls LinkedList01

---
+++ LinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList01
+{
+    class LinkedList
+    {
+        public class Node
+        {
+            //Wert
+            public object Value { get; set; }
+            //Link zum Nächsten
+            public Node Next { get; set; }
+
+            public Node() { }
+            public Node( object val )
+            {
+                this.Value = val;
+            }
+        }
+
+        //warum private?
+        private Node head;
+        int Count = 0;
+
+        public Node InsertAfter( Node prevNode, object value )
+        {
+            Node n = new Node( value );
+            if ( prevNode == null )
+                head = n;
+            else
+            {
+                //übernehme next vom Vorgänger
+                n.Next = prevNode.Next;
+                //werde next vom Vorgänger
+                prevNode.Next = n;
+            }
+            Count++;
+            return n;
+        }
+
+        public void DeleteAfter( Node prevNode )
+        {
+            //wenn kein prevNode übergeben wird, dann wird der erste gelöscht
+            if ( prevNode == null )
+            {
+                // Erster Knoten soll entfernt werden
+                if ( head != null )
+                    head = head.Next;
+            }
+            else
+            {
+                //nicht übergebene wird gelöscht, sondern after
+                Node nodeToBeDeleted = prevNode.Next;
+                if ( nodeToBeDeleted != null )
+                {
+                    Node newNext = nodeToBeDeleted.Next;
+                    prevNode.Next = newNext;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine( "--- Linked List ---" );
+            Node n = head;
+            while ( n != null )
+            {
+                Console.WriteLine( n.Value );
+                n = n.Next;
+            }
+        }
+
+        public bool isEmpty()
+        {
+            return (head == null);
+        }
+
+        public int Size()
+        {
+            return Count;
+        }
+
+        public void Push(object value)
+        {
+            if(head == null)
+            {
+                head = new Node(value);
+            }
+            else
+            {
+                Node temp = head;
+                head = new Node(value);
+                head.Next = temp;
+            }
+            Count++;
+        }
+
+        public void pop()
+        {
+            if(head != null)
+            {
+                head = head.Next;
+                Count--;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList01
+{
+    class Program
+    {
+        static void Main( string [] args )
+        {
+            LinkedList l = new LinkedList();
+            LinkedList.Node n1 = l.InsertAfter( null, 111 ); // An den Anfang
+            l.Print();
+
+            LinkedList.Node n2 = l.InsertAfter( n1, 222 ); // Hinter 111
+            l.Print();
+
+            LinkedList.Node n3 = l.InsertAfter( n2, 333 ); // Hinter 222
+            l.Print();
+
+            LinkedList.Node n2b = l.InsertAfter( n2, 299 ); //Zwischen 222 und 333
+            l.Print();
+
+            l.DeleteAfter( n1 ); // Entferne 222 (verbindet 111 und 299)
+            l.Print();
+
+            l.DeleteAfter( null ); // Entferne 111, erstes Element nun 299
+            l.Print();
+
+            l.DeleteAfter( n3 ); // Entfernt nichts, weil 333 das letzte Element ist
+            l.Print();
+
+            l.DeleteAfter( null ); // Entfernt ersten Knoten (299), nun ist 333 das erste (und einzige) Element
+            l.Print();
+
+            l.DeleteAfter( null ); // 333 wird auch entfernt
+            l.Print(); // Liste ist nun leer
+
+            Console.WriteLine("------------------------");
+            l.Push(6);
+            l.Print();
+            l.Push(7);
+            l.Push(1);
+            l.Print();
+
+            l.pop();
+            l.Print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
